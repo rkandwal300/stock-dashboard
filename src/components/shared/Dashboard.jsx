fd8e7b4ae09a5c1c25b42c6d2c41e3c833a8d7e9
Rahul Kandwal <rahulkandwal707@gmil.com>
2025-05-08 17:20:28 +0530

    eat(overview): added component

--- src/components/shared/Dashboard.jsx
+++ src/components/shared/Dashboard.jsx
@@ -3,17 +3,24 @@ import Header from "./header/Header";
 import { Card } from "../ui/card";
 import { mockCompanyDetails } from "@/constants/mock";
 import Details from "./details/Details";
+import Overview from "./overview/overview";
 
 export default function Dashboard() {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10">
+    <div className="h-screen  grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10">
       <Header name={mockCompanyDetails.name} />
       <div className="row-span-4 md:col-span-2">
         <Card className={"h-full"}>Chart</Card>
       </div>
 
       <div className="h-full">
-        <Card className={"h-full"}>Overview</Card>
+        <Overview
+          symbol={mockCompanyDetails.ticker}
+          price={300}
+          change={30}
+          changePercentage={10.0}
+          currency={"USD"}
+        />
       </div>
       <div className="row-span-2 md:col-span-1">
         <Details details={mockCompanyDetails} />
---
+++ src/components/shared/overview/overview.jsx
@@ -0,0 +1,32 @@
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import React from "react";
+
+function Overview({ symbol, price, change, changePercentage, currency }) {
+  return (
+    <Card className="p-4 py-8">
+      <span className="left-4 top-4 text-primary/70 font-medium text-lg xl:text-xl 2xl:text-2xl">
+        {symbol}
+      </span>
+      <div className="w-full h-full flex items-center justify-around">
+        <span className="text-2xl xl:text-4xl 2xl:text-5xl items-center">
+          ${price}
+          <span className="text-lg xl:text-xl 2xl:text-2xl text-muted-foreground m-2">
+            {currency}
+          </span>
+        </span>
+        <span
+          className={cn(
+            "text-lg xl:text-xl 2xl:text-2xl",
+            change >= 0 ? "text-lime-500" : "text-destructive"
+          )}
+        >
+          {change}
+          <span>({changePercentage || 0}%)</span>
+        </span>
+      </div>
+    </Card>
+  );
+}
+
+export default Overview;
