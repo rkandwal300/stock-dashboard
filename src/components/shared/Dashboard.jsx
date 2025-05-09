e4781cec4a69802c8bc4c8c443432a72273bff1d
Rahul Kandwal <rahulkandwal707@gmil.com>
2025-05-09 09:30:59 +0530

    feat(darkMode): added dark mode functionality

--- src/components/shared/Dashboard.jsx
+++ src/components/shared/Dashboard.jsx
@@ -7,7 +7,7 @@ import Overview from "./overview/overview";
 
 export default function Dashboard() {
   return (
-    <div className="h-screen  grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10">
       <Header name={mockCompanyDetails.name} />
       <div className="row-span-4 md:col-span-2">
         <Card className={"h-full"}>Chart</Card>
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Search from "./Search";
+import { useTheme } from "@/lib/context/ThemeContext";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
 
 function Header({ name }) {
+  
+  const { theme, toggleTheme } = useTheme()
   return (
-    <header className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
-      <div className="xl:px-32">
+    <header className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-between md:items-center items-start">
+      <div className="#xl:px-32">
         <h1 className="text-5xl font-medium">{name}</h1>
         <Search />
       </div>
-      {/* <ThemeIcon /> */}
+      <Button onClick={toggleTheme} variant="ghost" size="icon" className={''}>
+      {theme === "dark" ? <Sun className="h-5 w-5" /> : <Moon className="h-5 w-5" />}
+      <span className="sr-only">Toggle Theme</span>
+    </Button>
     </header>
   );
 }
